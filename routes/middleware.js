64d880f683723c2d37316542f494bfbc52f6b840
Joakim Monstad <jmonstad@cimpress.com>
2014-12-08 16:27:15 -0500

    messing around

--- routes/middleware.js
+++ routes/middleware.js
@@ -28,6 +28,7 @@ exports.initLocals = function(req, res, next) {
 	locals.navLinks = [
 		{ label: 'Home',		key: 'home',		href: '/' },
 		{ label: 'Blog',		key: 'blog',		href: '/blog' },
+                { label: 'Art',                 key: 'art',             href: '/art' },
 		{ label: 'Gallery',		key: 'gallery',		href: '/gallery' },
 		{ label: 'Contact',		key: 'contact',		href: '/contact' }
 	];
