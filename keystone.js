dfefba9ee2be37dd200b87b1047297dd93b513b6
Joakim Monstad <jmonstad@wlt041202.vistaprint.net>
2017-07-28 15:04:03 -0400

    upgrade nunjucks. new version has watch off by default, which should fix bug. need to disable autoescape

--- keystone.js
+++ keystone.js
@@ -4,32 +4,34 @@ require('dotenv').load();
 
 // Require keystone
 var keystone = require('keystone'),
-	cons = require('consolidate'),
-	nunjucks = require('nunjucks');
+    cons = require('consolidate'),
+    nunjucks = require('nunjucks');
 
 // Initialise Keystone with your project's configuration.
 // See http://keystonejs.com/guide/config for available options
 // and documentation.
 
+nunjucks.configure({ autoescape: false });
+
 keystone.init({
 
-	'name': 'babacool',
-	'brand': 'babacool',
-	
-	'less': 'public',
-	'static': 'public',
-	'favicon': 'public/favicon.ico',
-	'views': 'templates/views',
-	'view engine': 'html',
-	'custom engine': cons.nunjucks,
-	
-	'emails': 'templates/emails',
-	
-	'auto update': true,
-	'session': true,
-	'auth': true,
-	'user model': 'User',
-	'cookie secret': '4SwmcuG9qZa43,ekiiW$%M?L3Czhc?l?wlM]^3U$]gt8o3?ztV:n0!pZ{r_/gNme'
+    'name': 'babacool',
+    'brand': 'babacool',
+
+    'less': 'public',
+    'static': 'public',
+    'favicon': 'public/favicon.ico',
+    'views': 'templates/views',
+    'view engine': 'html',
+    'custom engine': cons.nunjucks,
+
+    'emails': 'templates/emails',
+
+    'auto update': true,
+    'session': true,
+    'auth': true,
+    'user model': 'User',
+    'cookie secret': '4SwmcuG9qZa43,ekiiW$%M?L3Czhc?l?wlM]^3U$]gt8o3?ztV:n0!pZ{r_/gNme'
 
 });
 
@@ -42,10 +44,10 @@ keystone.import('models');
 // for each request) should be added to ./routes/middleware.js
 
 keystone.set('locals', {
-	_: require('underscore'),
-	env: keystone.get('env'),
-	utils: keystone.utils,
-	editable: keystone.content.editable
+    _: require('underscore'),
+    env: keystone.get('env'),
+    utils: keystone.utils,
+    editable: keystone.content.editable
 });
 
 // Load your project's Routes
@@ -56,18 +58,18 @@ keystone.set('routes', require('./routes'));
 // default email templates, you may remove them if you're using your own.
 
 keystone.set('email locals', {
-	logo_src: '/images/logo-email.gif',
-	logo_width: 194,
-	logo_height: 76,
-	theme: {
-		email_bg: '#f9f9f9',
-		link_color: '#2697de',
-		buttons: {
-			color: '#fff',
-			background_color: '#2697de',
-			border_color: '#1a7cb7'
-		}
-	}
+    logo_src: '/images/logo-email.gif',
+    logo_width: 194,
+    logo_height: 76,
+    theme: {
+        email_bg: '#f9f9f9',
+        link_color: '#2697de',
+        buttons: {
+            color: '#fff',
+            background_color: '#2697de',
+            border_color: '#1a7cb7'
+        }
+    }
 });
 
 // Setup replacement rules for emails, to automate the handling of differences
@@ -91,10 +93,10 @@ keystone.set('email tests', require('./routes/emails'));
 // Configure the navigation bar in Keystone's Admin UI
 
 keystone.set('nav', {
-	'posts': ['posts', 'post-categories'],
-	'galleries': 'galleries',
-	'enquiries': 'enquiries',
-	'users': 'users'
+    'posts': ['posts', 'post-categories'],
+    'galleries': 'galleries',
+    'enquiries': 'enquiries',
+    'users': 'users'
 });
 
 // Start Keystone to connect to your database and initialise the web server
